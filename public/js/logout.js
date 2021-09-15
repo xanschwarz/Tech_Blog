e210a1b81671342a0b80cc0bbbed3af522d07912
Xander <xanschwarz@gmail.com>
2021-09-14 17:44:17 -0700

    Added files from class activity. Some edits to set up for this app.

---
+++ public/js/logout.js
@@ -0,0 +1,18 @@
+/*  ----------------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------  TAKEN FROM AN ACTIVITY, HASN'T BEEN EDITED YET  ---------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------  */
+
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
