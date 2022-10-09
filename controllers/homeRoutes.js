60ca211478ac52fd9ff1eab31952b468796f2c21
Xander <xanschwarz@gmail.com>
2022-10-09 12:13:38 -0700

    Clicking on a post from home will now redirect to that specific post's route, which will render only that post. Have not yet added comments and new comment form to that view.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -58,7 +58,7 @@ router.get('/post/:id', async (req, res) => {
 
     const post = postData.get({ plain: true });
 
-    res.render('single-post', { post });
+    res.render('single_post', { post });
   } catch (err) {
     res.status(500).json(err);
   }
